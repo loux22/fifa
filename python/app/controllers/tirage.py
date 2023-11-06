153260be20feb75f8518e6d9cee103d4ac1c1e10
loux22 <louis.ardilly@yahoo.fr>
2023-11-06 17:55:57 +0100

    euro fonctionnel

--- python/app/controllers/tirage.py
+++ python/app/controllers/tirage.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 import mysql.connector
+import random
 
 
 fifa = Blueprint('fifa', __name__, url_prefix='/')
@@ -12,6 +13,7 @@ def index():
 
 @fifa.route('/tirage', methods=['GET', 'POST'])
 def tirage():
+    chapeau = []
     continent = request.args.get('continent')
 
     teams = get_team_filtre()
@@ -21,10 +23,17 @@ def tirage():
     if not continent == "tous" :
         teams = [dico for dico in teams if dico.get('continent') == continent]
 
-    teams_select = get_team_select()
-    print(teams_select)
+    if request.method == 'POST':
+        n_team = int(request.form.get('n_team'))
+        team_select = request.form.getlist('team_select')
+        for i in range (0, int(n_team/4)):
+            chapeau.append(team_select[4*i:4*(i+1)]) 
 
-    return render_template('pages/tirage.html', teams=teams, continent=continent)
+    poules = [chap.copy() for chap in chapeau]
+    for chap in poules:
+        random.shuffle(chap)
+
+    return render_template('pages/tirage.html', teams=teams, continent=continent, chapeau=chapeau, poules=poules)
 
 
 
@@ -51,17 +60,3 @@ def get_team_filtre():
     else:
         print("Could not connect")
     return teams
-
-def get_team_select():
-    teams_select = []
-    if request.method == 'POST':
-        n_team = request.form.get('n_team')
-        team_select = request.form.getlist('team_select')
-        for team in team_select:
-            cur = connexion_bdd()
-            with cur.cursor(dictionary=True) as cursor:
-                result = cursor.execute(f"SELECT * FROM equipe WHERE nom_equipe='{team}'")
-                team_tirage = cursor.fetchone()
-                teams_select.append(team_tirage)
-        cur.close()
-    return teams_select
